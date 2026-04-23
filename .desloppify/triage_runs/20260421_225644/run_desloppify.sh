986775b8f9603e366ef4569597e42d42ee613fe5
Ralf Waldukat <ralf.waldukat@gmail.com>
2026-04-23 12:41:32 +0700

    chore: remove desloppify artifacts

--- .desloppify/triage_runs/20260421_225644/run_desloppify.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-export PYTHONPATH=/Users/avion/.local/pipx/venvs/desloppify/lib/python3.11/site-packages${PYTHONPATH:+:$PYTHONPATH}
-exec /Users/avion/.local/pipx/venvs/desloppify/bin/python -m desloppify.cli "$@"
